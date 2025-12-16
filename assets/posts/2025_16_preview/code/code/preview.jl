e439dc37aa58bcbf2bc9aa03902fb22e671c63bd
KyleSJohnston <KyleSJohnston@users.noreply.github.com>
2025-12-16 15:00:51 +0000

    Deploying to gh-pages from @ football-town/football-town.github.io@09d81b8b0d364310791fcc1789055922c921c4b8 🚀

---
+++ assets/posts/2025_16_preview/code/code/preview.jl
@@ -0,0 +1,26 @@
+# This file was generated, do not modify it. # hide
+#hideall
+using Chain
+using DataFrames
+using Dates
+using NFLData
+using Printf
+
+include("src/common.jl")
+
+@chain load_schedules() begin
+    subset(
+        :season => x -> x .== 2025,
+        :week => x -> x .== 16,
+    )
+    sort([
+        :gameday,
+        :gametime,
+    ])
+    select(
+        [:weekday, :gametime] => ((wd, gt) -> stringify_time.(wd, gt)) => :when,
+        :spread_line => (x -> stringify_spread.(x)) => :spread,
+        [:away_team, :home_team] => ((a,h) -> stringify_game.(a,h)) => :description,
+        :total_line => :total,
+    )
+end
